c35f18195f3187ff578109d3922bebcff7e8820d
Guillermo Theler <guillermot@onesolutions.com.ar>
2022-12-30 09:53:48 -0300

    arreglos en diff

--- jdbcdiff/src/main/java/com/gt/jdbcdiff/DbDiff.java
+++ jdbcdiff/src/main/java/com/gt/jdbcdiff/DbDiff.java
@@ -8,51 +8,78 @@ import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DbDiff {
 
-	Connection c1;
-	Connection c2;
+	Connection dbConn1;
+	Connection dbConn2;
 
 	OutputStream outstream;
 
-	public DbDiff(Connection c1, Connection c2, OutputStream outStream) {
+	public DbDiff(Connection dbConn1, Connection dbConn2, OutputStream outStream) {
 		super();
-		this.c1 = c1;
-		this.c2 = c2;
+		this.dbConn1 = dbConn1;
+		this.dbConn2 = dbConn2;
 		this.outstream = outStream;
 	}
 
 	public void comparar() {
 		try {
+			Logger.getLogger(getClass().getName()).log(Level.INFO,
+					"INICIO comparacion " + dbConn1.getMetaData().getURL() + " con " + dbConn2.getMetaData().getURL());
+
 			write("\n\n");
-			write("-- INICIO comparacion " + c1.getMetaData().getURL() + " con " + c2.getMetaData().getURL());
+			write("-- INICIO comparacion " + getConnInfo(dbConn1) + " con " + getConnInfo(dbConn2));
+			write("\n");
+			write("-- CAMBIOS a aplicar en " + getConnInfo(dbConn2) + " para que coincida con " + getConnInfo(dbConn1));
 			write("\n\n");
 
-			diffTablas(c1, c2);
+			diffTablas(dbConn1, dbConn2);
 
-			write("-- Fin comparacion " + c1.getMetaData().getURL() + " con " + c2.getMetaData().getURL());
+			write("-- Fin comparacion ");
 			write("\n\n\n\n");
+			Logger.getLogger(getClass().getName()).log(Level.INFO, "Fin comparación");
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			Logger.getLogger(getClass().getName()).log(Level.SEVERE, "Error en DB comparando jdbc conn", e);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			Logger.getLogger(getClass().getName()).log(Level.SEVERE, "Error IO comparando jdbc conn", e);
 		}
+	}
+
+	private String getConnInfo(Connection conn) throws SQLException {
+		// StringBuilder sb = new StringBuilder();
+
+		// String catalog = conn.getCatalog();
+
+		// sb.append(catalog);
+
+		// ResultSet schemasRs = conn.getMetaData().getSchemas();
+
+		// while (schemasRs.next()) {
+		// for (int i = 1; i <= schemasRs.getMetaData().getColumnCount(); i++) {
+		// String colName = schemasRs.getMetaData().getColumnName(i);
+		// String colValue =
+		// Optional.ofNullable(schemasRs.getObject(colName)).orElse("").toString();
+		// sb.append("[" + colName + " = '" + colValue + "'']");
+		// }
+		// }
+
+		return conn.getCatalog();
 	}
 
 	private void diffTablas(Connection izquierda, Connection derecha) throws SQLException, IOException {
 
-		ResultSet rs1 = this.c1.getMetaData().getTables(this.c1.getCatalog(), null, "%", new String[] { "TABLE" });
+		ResultSet rs1 = this.dbConn1.getMetaData().getTables(this.dbConn1.getCatalog(), null, "%",
+				new String[] { "TABLE" });
 		ResultSet rs2;
 
 		String tabla1;
 		while (rs1.next()) {
 			tabla1 = rs1.getString("TABLE_SCHEM") + "." + rs1.getString("TABLE_NAME");
-			rs2 = this.c2.getMetaData().getTables(this.c2.getCatalog(), null, "%", new String[] { "TABLE" });
+			rs2 = this.dbConn2.getMetaData().getTables(this.dbConn2.getCatalog(), null, "%", new String[] { "TABLE" });
 			boolean esta = false;
 			while (rs2.next()) {
 				if (tabla1.equalsIgnoreCase(rs2.getString("TABLE_SCHEM") + "." + rs2.getString("TABLE_NAME"))) {
@@ -60,29 +87,32 @@ public class DbDiff {
 					break;
 				}
 			}
+
 			if (!esta) {
 				getCreateScript(izquierda, rs1.getString("TABLE_SCHEM"), rs1.getString("TABLE_NAME"));
 			} else {
 				// comparo los campos
-				compararCampos(izquierda, derecha, rs1.getString("TABLE_SCHEM"), rs1.getString("TABLE_NAME"));
+				updateCreateCampos(izquierda, derecha, rs1.getString("TABLE_SCHEM"), rs1.getString("TABLE_NAME"));
 			}
+
+			deleteCampos(izquierda, derecha, rs1.getString("TABLE_SCHEM"), rs1.getString("TABLE_NAME"));
 		}
 	}
 
-	public Connection getC1() {
-		return c1;
+	public Connection getDbConn1() {
+		return dbConn1;
 	}
 
-	public void setC1(Connection c1) {
-		this.c1 = c1;
+	public void setDbConn1(Connection c1) {
+		this.dbConn1 = c1;
 	}
 
-	public Connection getC2() {
-		return c2;
+	public Connection getDbConn2() {
+		return dbConn2;
 	}
 
-	public void setC2(Connection c2) {
-		this.c2 = c2;
+	public void setDbConn2(Connection c2) {
+		this.dbConn2 = c2;
 	}
 
 	private void write(String content) {
@@ -93,7 +123,8 @@ public class DbDiff {
 		}
 	}
 
-	private void compararCampos(Connection izquierda, Connection derecha, String schemaName, String tableName) throws SQLException {
+	private void updateCreateCampos(Connection izquierda, Connection derecha, String schemaName, String tableName)
+			throws SQLException {
 
 		DatabaseMetaData metaData1 = izquierda.getMetaData();
 
@@ -101,9 +132,8 @@ public class DbDiff {
 
 		DatabaseMetaData metaData2 = derecha.getMetaData();
 
-		ResultSet rs2 = metaData2.getColumns(derecha.getCatalog(), schemaName, tableName, "%");
-
 		while (rs1.next()) {
+			ResultSet rs2 = metaData2.getColumns(derecha.getCatalog(), schemaName, tableName, "%");
 			boolean esta = false;
 			while (rs2.next()) {
 				if (rs1.getString("COLUMN_NAME").equalsIgnoreCase(rs2.getString("COLUMN_NAME"))) {
@@ -119,30 +149,59 @@ public class DbDiff {
 			} else {
 				// me fijo que sean del mismo tipo
 				if (!getTipoSQL(rs1).equalsIgnoreCase(getTipoSQL(rs2))) {
-					write("-- cambio de tipo campo " + rs1.getString("COLUMN_NAME") + "\n");
+					write("-- cambio de tipo, campo " + rs1.getString("COLUMN_NAME") + "\n");
 					write("ALTER TABLE " + schemaName + "." + tableName);
-					
+
 					String originalURL = derecha.getMetaData().getURL();
 					Driver drv = DriverManager.getDriver(originalURL);
 					String driverClass = drv.getClass().getName();
-					
-					
+
 					switch (driverClass) {
-					case "com.microsoft.sqlserver.jdbc.SQLServerDriver":
-						write(" ALTER COLUMN " + rs1.getString("COLUMN_NAME"));
-						write(" " + getTipoSQL(rs1));
-						break;
-					case "org.postgresql.Driver":
-						write(" ALTER COLUMN " + rs1.getString("COLUMN_NAME"));
-						write(" TYPE " + getTipoSQL(rs1));
-						break;
-					default:
-						write(" MODIFY COLUMN " + rs1.getString("COLUMN_NAME"));
-						write(" " + getTipoSQL(rs1));
+						case "com.microsoft.sqlserver.jdbc.SQLServerDriver":
+							write(" ALTER COLUMN " + rs1.getString("COLUMN_NAME"));
+							write(" " + getTipoSQL(rs1));
+							break;
+						case "org.postgresql.Driver":
+							write(" ALTER COLUMN " + rs1.getString("COLUMN_NAME"));
+							write(" TYPE " + getTipoSQL(rs1));
+							break;
+						default:
+							write(" MODIFY COLUMN " + rs1.getString("COLUMN_NAME"));
+							write(" " + getTipoSQL(rs1));
 					}
 					write(";\n");
 
 				}
+			}
+		}
+	}
+
+	private void deleteCampos(Connection izquierda, Connection derecha, String schemaName, String tableName)
+			throws SQLException {
+
+		DatabaseMetaData metaData1 = derecha.getMetaData();
+
+		ResultSet rs1 = metaData1.getColumns(derecha.getCatalog(), schemaName, tableName, "%");
+
+		DatabaseMetaData metaData2 = izquierda.getMetaData();
+
+		while (rs1.next()) {
+			ResultSet rs2 = metaData2.getColumns(izquierda.getCatalog(), schemaName, tableName, "%");
+			boolean esta = false;
+			while (rs2.next()) {
+				if (rs1.getString("COLUMN_NAME").equalsIgnoreCase(rs2.getString("COLUMN_NAME"))) {
+					esta = true;
+					break;
+				}
+			}
+
+			if (!esta) {
+				// me fijo que sean del mismo tipo
+				write("-- sobra campo " + rs1.getString("COLUMN_NAME") + "\n");
+				write("-- ALTER TABLE " + schemaName + "." + tableName);
+				write(" DROP COLUMN " + rs1.getString("COLUMN_NAME"));
+
+				write(";\n");
 			}
 		}
 	}
@@ -164,12 +223,14 @@ public class DbDiff {
 			boolean first = true;
 
 			boolean tieneIsAutoinc = false;
+
 			for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
 				if (rs.getMetaData().getColumnName(i).equalsIgnoreCase("IS_AUTOINCREMENT")) {
 					tieneIsAutoinc = true;
 					break;
 				}
 			}
+
 			while (rs.next()) {
 				if (first) {
 					first = false;
@@ -202,32 +263,40 @@ public class DbDiff {
 	}
 
 	private void fieldDef(Connection connection, ResultSet rs, boolean tieneIsAutoinc) throws SQLException {
+
+		// LOG DE LOS CAMPOS DE LOS METADATOS DE UNA COLUMNA
+
+		// StringBuilder sb = new StringBuilder();
+		// for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+		// sb.append("[" + rs.getMetaData().getColumnName(i) + "] = " +
+		// Optional.ofNullable(rs.getObject(rs.getMetaData().getColumnName(i))).orElse("NULL").toString()
+		// + "\n");
+		// }
+		// Logger.getLogger(getClass().getName()).log(Level.INFO, sb.toString());
+
+		// LOG DE LOS CAMPOS DE LOS METADATOS DE UNA COLUMNA
+
 		write(rs.getString("COLUMN_NAME"));
 		write(" ");
+
 		if ((tieneIsAutoinc) && (rs.getString("IS_AUTOINCREMENT") != null)
 				&& (rs.getString("IS_AUTOINCREMENT").equalsIgnoreCase("yes"))) {
 			switch (connection.getClass().getName()) {
-			case "com.microsoft.sqlserver.jdbc.SQLServerDriver":
-				write("IDENTITY(1,1)");
-				break;
-			case "org.postgresql.Driver":
-				write("SERIAL");
-				break;
-			default:
-				write(rs.getString("TYPE_NAME"));
-				write("(");
-				write(rs.getInt("COLUMN_SIZE") + "");
+				case "com.microsoft.sqlserver.jdbc.SQLServerDriver":
+					write("IDENTITY(1,1)");
+					break;
+				case "org.postgresql.Driver":
+					write("SERIAL");
+					break;
+				default:
+					write(rs.getString("TYPE_NAME"));
+					write("(");
+					write(rs.getInt("COLUMN_SIZE") + "");
 			}
 		} else {
 			String tipoSQL = getTipoSQL(rs);
 
 			write(tipoSQL);
-
-			if (tipoSQL.equals("VARCHAR")) {
-				write("(");
-				write(rs.getInt("COLUMN_SIZE") + "");
-				write(")");
-			}
 		}
 		if (rs.getInt("NULLABLE") == 0) {
 			write(" NOT NULL");
@@ -248,6 +317,16 @@ public class DbDiff {
 			tipoSQL = "BYTEA";
 		} else if (tipoSQL.equals("DOUBLE")) {
 			tipoSQL = "DOUBLE PRECISION";
+		}
+
+		if (tipoSQL.equalsIgnoreCase("VARCHAR") || tipoSQL.equalsIgnoreCase("NVARCHAR")) {
+			tipoSQL += "(";
+			if (rs.getInt("COLUMN_SIZE") == Integer.MAX_VALUE) {
+				tipoSQL += "MAX";
+			} else {
+				tipoSQL += rs.getInt("COLUMN_SIZE") + "";
+			}
+			tipoSQL += ")";
 		}
 
 		return tipoSQL;
--- jdbcdiff/src/main/java/com/gt/jdbcdiff/MainClass.java
+++ jdbcdiff/src/main/java/com/gt/jdbcdiff/MainClass.java
@@ -2,12 +2,15 @@ package com.gt.jdbcdiff;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URISyntaxException;
 import java.security.CodeSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,18 +24,24 @@ import org.apache.commons.cli.ParseException;
 
 public class MainClass {
 
-	public static void main(String[] args) throws SQLException, ClassNotFoundException, FileNotFoundException, IOException {
+	public static void main(String[] args)
+			throws SQLException, ClassNotFoundException, FileNotFoundException, IOException {
 
 		if (args == null || args.length == 0) {
-			args = new String[] { 
-					"-d1", "org.postgresql.Driver", 
-					"-d2", "org.postgresql.Driver", 
-					"-j1", "jdbc:postgresql://192.168.80.5/gcem_pos", 
-					"-j2", "jdbc:postgresql://192.168.80.5/gcem_pos_viejo", 
-					"-u1", "carnave", 
-					"-u2", "carnave", 
-					"-p1", "Quilmes", 
-					"-p2", "Quilmes"};
+			args = new String[] {
+					"-d1", "com.microsoft.sqlserver.jdbc.SQLServerDriver",
+					"-d2", "com.microsoft.sqlserver.jdbc.SQLServerDriver",
+					"-j1",
+					"jdbc:sqlserver://;serverName=dev.onesolutions.com.ar;databaseName=OSDEV_DEV;TrustServerCertificate=True;",
+					"-j2",
+					"jdbc:sqlserver://;serverName=dev.onesolutions.com.ar;databaseName=OSTest_QA;TrustServerCertificate=True;",
+					"-u1", "luisB",
+					"-u2", "luisB",
+					"-p1", "293!pMr7",
+					"-p2", "293!pMr7",
+					"-f1", "diff1.sql",
+					"-f2", "diff2.sql"
+			};
 		}
 
 		CommandLineParser parser = new DefaultParser();
@@ -51,19 +60,40 @@ public class MainClass {
 			Class.forName(commandLine.getOptionValue("d1"));
 			Class.forName(commandLine.getOptionValue("d2"));
 
-			Connection c1 = DriverManager.getConnection(commandLine.getOptionValue("j1"), commandLine.getOptionValue("u1"),
+			Connection dbConn1 = DriverManager.getConnection(commandLine.getOptionValue("j1"),
+					commandLine.getOptionValue("u1"),
 					commandLine.getOptionValue("p1"));
-			Connection c2 = DriverManager.getConnection(commandLine.getOptionValue("j2"), commandLine.getOptionValue("u2"),
+			Connection dbConn2 = DriverManager.getConnection(commandLine.getOptionValue("j2"),
+					commandLine.getOptionValue("u2"),
 					commandLine.getOptionValue("p2"));
 
-			DbDiff dbDiff = new DbDiff(c1, c2, System.out);
-			dbDiff.comparar();
-			
-			dbDiff = new DbDiff(c2, c1, System.out);
+			OutputStream file1 = null;
+			OutputStream file2 = null;
+
+			if (commandLine.hasOption("file1")) {
+				file1 = new FileOutputStream(commandLine.getOptionValue("file1"));
+			}
+
+			if (commandLine.hasOption("file2")) {
+				file2 = new FileOutputStream(commandLine.getOptionValue("file2"));
+			}
+
+			DbDiff dbDiff = new DbDiff(dbConn1, dbConn2, Optional.ofNullable(file1).orElse(System.out));
 			dbDiff.comparar();
 
-			c1.close();
-			c2.close();
+			dbDiff = new DbDiff(dbConn2, dbConn1, Optional.ofNullable(file2).orElse(System.out));
+			dbDiff.comparar();
+
+			dbConn1.close();
+			dbConn2.close();
+
+			if (file1 != null) {
+				file1.close();
+			}
+
+			if (file2 != null) {
+				file2.close();
+			}
 		}
 	}
 
@@ -73,13 +103,17 @@ public class MainClass {
 
 		options.addOption(Option.builder("h").longOpt("help").argName("comando").build());
 
-		options.addOption(Option.builder("d1").longOpt("driverclass1").hasArg().desc("Clase java del driver jdbc conexión 1").build());
-		options.addOption(Option.builder("j1").longOpt("jdbc1").hasArg().desc("jdbc connection string conexión 1").build());
+		options.addOption(Option.builder("d1").longOpt("driverclass1").hasArg()
+				.desc("Clase java del driver jdbc conexión 1").build());
+		options.addOption(
+				Option.builder("j1").longOpt("jdbc1").hasArg().desc("jdbc connection string conexión 1").build());
 		options.addOption(Option.builder("u1").longOpt("uid1").hasArg().desc("Nombre de usuario conexión 1").build());
 		options.addOption(Option.builder("p1").longOpt("pass1").hasArg().desc("contraseña conexión 1").build());
 		options.addOption(Option.builder("f1").longOpt("file1").hasArg().desc("base de datos conexión 1").build());
-		options.addOption(Option.builder("d2").longOpt("driverclass2").hasArg().desc("Clase java del driver jdbc conexión 2").build());
-		options.addOption(Option.builder("j2").longOpt("jdbc2").hasArg().desc("jdbc connection string conexión 2").build());
+		options.addOption(Option.builder("d2").longOpt("driverclass2").hasArg()
+				.desc("Clase java del driver jdbc conexión 2").build());
+		options.addOption(
+				Option.builder("j2").longOpt("jdbc2").hasArg().desc("jdbc connection string conexión 2").build());
 		options.addOption(Option.builder("u2").longOpt("uid2").hasArg().desc("Nombre de usuario conexión 2").build());
 		options.addOption(Option.builder("p2").longOpt("pass2").hasArg().desc("contraseña conexión 2").build());
 		options.addOption(Option.builder("f2").longOpt("file2").hasArg().desc("base de datos conexión 2").build());
@@ -95,7 +129,8 @@ public class MainClass {
 			File jarFile = new File(codeSource.getLocation().toURI().getPath());
 			jarDir = jarFile.getParentFile().getPath();
 		} catch (URISyntaxException ex) {
-			java.util.logging.Logger.getLogger(MainClass.class.getName()).log(java.util.logging.Level.SEVERE, "Error buscando jar path", ex);
+			java.util.logging.Logger.getLogger(MainClass.class.getName()).log(java.util.logging.Level.SEVERE,
+					"Error buscando jar path", ex);
 		}
 		return jarDir;
 	}
